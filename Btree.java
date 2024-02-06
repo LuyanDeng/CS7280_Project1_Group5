e4fcab597ba744ecf89628657c5aeb46d264949a
CarolineFang1998 <f767475251@gmail.com>
2024-02-06 10:34:18 -0800

    init

---
+++ Btree.java
@@ -0,0 +1,168 @@
+/*
+ * CS7280 Special Topics in Database Management
+ * Project 1: B-tree implementation.
+ *
+ * You need to code for the following functions in this program
+ *   1. Lookup(int value) -> nodeLookup(int value, int node)
+ *   2. Insert(int value) -> nodeInsert(int value, int node)
+ *   3. Display(int node)
+ *
+ */
+
+final class Btree {
+
+  /* Size of Node. */
+  private static final int NODESIZE = 5;
+
+  /* Node array, initialized with length = 1. i.e. root node */
+  private Node[] nodes = new Node[1];
+
+  /* Number of currently used nodes. */
+  private int cntNodes;
+
+  /* Pointer to the root node. */
+  private int root;
+
+  /* Number of currently used values. */
+  private int cntValues;
+
+  /*
+   * B+ tree Constructor.
+   */
+  public Btree() {
+    root = initNode();
+    nodes[root].children[0] = createLeaf();
+  }
+
+  /*********** B tree functions for Public ******************/
+
+  /*
+   * Lookup(int value)
+   *   - True if the value was found.
+   */
+  public boolean Lookup(int value) {
+    return nodeLookup(value, root);
+  }
+
+  /*
+   * Insert(int value)
+   *    - If -1 is returned, the value is inserted and increase cntValues.
+   *    - If -2 is returned, the value already exists.
+   */
+  public void Insert(int value) {
+    if(nodeInsert(value, root) == -1) cntValues++;
+  }
+
+
+  /*
+   * CntValues()
+   *    - Returns the number of used values.
+   */
+  public int CntValues() {
+    return cntValues;
+  }
+
+  /*********** B-tree functions for Internal  ******************/
+
+  /*
+   * nodeLookup(int value, int pointer)
+   *    - True if the value was found in the specified node.
+   *
+   */
+  private boolean nodeLookup(int value, int pointer) {
+    //
+    //
+    // TODO
+    //
+    //
+    return XXX;
+  }
+
+  /*
+   * nodeInsert(int value, int pointer)
+   *    - -2 if the value already exists in the specified node
+   *    - -1 if the value is inserted into the node or
+   *            something else if the parent node has to be restructured
+   */
+  private int nodeInsert(int value, int pointer) {
+    //
+    //
+    // TODO
+    //
+    //
+    return XXX;
+  }
+
+
+  /*********** Functions for accessing node  ******************/
+
+  /*
+   * isLeaf(Node node)
+   *    - True if the specified node is a leaf node.
+   *         (Leaf node -> a missing children)
+   */
+  boolean isLeaf(Node node) {
+    return node.children == null; // TODO: check the children size is greater than 0
+  }
+
+  /*
+   * initNode(): Initialize a new node and returns the pointer.
+   *    - return node pointer
+   */
+  int initNode() {
+    Node node = new Node();
+    node.values = new int[NODESIZE];
+    node.children =  new int[NODESIZE + 1];
+
+    checkSize();
+    nodes[cntNodes] = node;
+    return cntNodes++;
+  }
+
+  /*
+   * createLeaf(): Creates a new leaf node and returns the pointer.
+   *    - return node pointer
+   */
+  int createLeaf() {
+    Node node = new Node();
+    node.values = new int[NODESIZE];
+
+    checkSize();
+    nodes[cntNodes] = node;
+    return cntNodes++;
+  }
+
+  /*
+   * checkSize(): Resizes the node array if necessary.
+   */
+  private void checkSize() {
+    // if node array is already full, temp will be cntNodes * 2
+    if(cntNodes == nodes.length) {
+      Node[] tmp = new Node[cntNodes << 1];
+      System.arraycopy(nodes, 0, tmp, 0, cntNodes);
+      nodes = tmp;
+    }
+  }
+}
+
+/*
+ * Node data structure.
+ *   - This is the simplest structure for nodes used in B-tree
+ *   - This will be used for both internal and leaf nodes.
+ */
+final class Node {
+  /* Node Values (Leaf Values / Key Values for the children nodes).  */
+  int[] values;
+
+  /* Node Array, pointing to the children nodes.
+   * This array is not initialized for leaf nodes.
+   */
+  int[] children;
+
+  /* Number of entries
+   * (Rule in B Trees:  d <= size <= 2 * d).
+   */
+  int size;
+
+  // TODO: need a constructor
+}
